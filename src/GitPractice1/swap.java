dfa44a906e73239f22ac652fad413eefb1e019e8
shaiknabee <138884531+shaiknabee@users.noreply.github.com>
2023-07-08 18:00:05 +0530

    commit1
    Swap program

---
+++ src/GitPractice1/swap.java
@@ -0,0 +1,15 @@
+package GitPractice1;
+
+public class swap
+{
+	public static void main(String[] args) 
+	{
+      int a=10,b=20,c;
+      c=a;
+      a=b;
+      b=c;
+      System.out.println("a= "+a+" & "+"b= "+b);
+    
+      
+	}
+}
