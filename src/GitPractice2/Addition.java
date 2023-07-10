5e20b14d0ec9a0f8270662b98cb1358722c6a466
shaiknabee <138884531+shaiknabee@users.noreply.github.com>
2023-07-10 16:51:06 +0530

    commit
    updated

--- src/GitPractice2/Addition.java
+++ src/GitPractice2/Addition.java
@@ -4,6 +4,6 @@ public class Addition
 {
 	public static void main(String[] args)
 	{
-        System.out.println("hi how are you,where are you going today");
+    System.out.println("hi how are you,where are you going today");
 	}
 }
